e92dc64009b41e4b402cf2c05e7e98d26d77dbe0
Siema Alam <siemaalam@gmail.com>
2022-12-09 04:13:48 -0500

    add minor design changes

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -4,42 +4,32 @@ function AboutUsPage(props) {
   return (
     <>
       <div className="col text-center">
-        <h2 className="mb-3">About our project</h2>
-        <p className="mb-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          quidem adipisci nobis quia eum quaerat quos ducimus, deleniti
-          exercitationem animi itaque iste illo reiciendis vitae atque
-          necessitatibus voluptatum repellendus quisquam?
-        </p>
-        <h2 className="mb-3">About our Team</h2>
+        <h1 className="mb-3">Our Mission</h1>
+        <h4 className="mb-5">
+          By matching users to a challenge with their friends,<br></br>
+          users will be able to bond over running and push themselves<br></br> to achieve their running goals.
+
+        </h4>
+        <h2 className="mb-3">Our Team</h2>
         <div className="row">
           <div className="col-lg-4">
             <h3>Siema Alam</h3>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Dignissimos in itaque nihil consectetur qui natus similique
-              nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
-              provident neque culpa alias incidunt amet.
+              Frontend Engineering
             </p>
             <a href="https://github.com/siemaaa" type="button" className="btn btn-outline-dark" role="button">GitHub</a>
           </div>
           <div className="col-lg-4">
             <h3>Farhanul Thouship</h3>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Dignissimos in itaque nihil consectetur qui natus similique
-              nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
-              provident neque culpa alias incidunt amet.
+              Middleware Engineering
             </p>
             <a href="https://github.com/thouship" type="button" className="btn btn-outline-dark" role="button">GitHub</a>
           </div>
           <div className="col-lg-4">
             <h3>Abdul Rafi</h3>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Dignissimos in itaque nihil consectetur qui natus similique
-              nostrum molestias, ipsa explicabo hic impedit aspernatur. Ipsa
-              provident neque culpa alias incidunt amet.
+              Backend Engineering
             </p>
             <a href="https://github.com/arafi6624" type="button" className="btn btn-outline-dark" role="button">GitHub</a>
           </div>
