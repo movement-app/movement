68fa33610b331dca158025916e3637111e2d22be
Siema Alam <siemaalam@gmail.com>
2022-12-09 03:40:37 -0500

    add modals to buttons on dash, update ui tracker

--- client/src/pages/RunTrackerPage.js
+++ client/src/pages/RunTrackerPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import RunItem from "../components/RunItem";
 import ErrorAlert from "../components/ErrorAlert";
 import LoadingSpinner from "../components/LoadingSpinner";
@@ -8,20 +8,20 @@ import LoadingSpinner from "../components/LoadingSpinner";
 function RunTrackerPage(props) {
 
   const [run, setRun] = useState([]);
-	const [totalRun, setTotalRun] = useState(0);
+  const [totalRun, setTotalRun] = useState(0);
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({ description: "", distance: "", startTime: "", endTime: "", date: ""});
+  const [data, setData] = useState({ description: "", distance: "", startTime: "", endTime: "", date: "" });
 
-	useEffect(() => {
-		let temp = 0;
-		for(let i = 0; i < run.length; i++) {
-			temp += parseInt(run[i].distance);
-		}
+  useEffect(() => {
+    let temp = 0;
+    for (let i = 0; i < run.length; i++) {
+      temp += parseInt(run[i].distance);
+    }
 
-		setTotalRun(temp);
-	}, [run]);
+    setTotalRun(temp);
+  }, [run]);
 
   const fieldChanged = (name) => {
     return (event) => {
@@ -82,7 +82,7 @@ function RunTrackerPage(props) {
 
   const removeRun = async (id) => {
     try {
-      const response = await fetch (`/api/logs/${id}`, { method: "delete" });
+      const response = await fetch(`/api/logs/${id}`, { method: "delete" });
 
       if (response.ok) {
         setSuccess(true);
@@ -108,21 +108,34 @@ function RunTrackerPage(props) {
       {error && <ErrorAlert details={"Failed to save the content"} />}
       <div className="col text-center">
         <form className="run-form" onSubmit={handleSubmit}>
-          <div className="form-inner">
-            <input type="text" name="desc" id="desc" placeholder="Run Description..." value={data.description} onChange={fieldChanged("description")}/> 
-            <input type="number" name="mileage" id="mileage" placeholder="Miles..." value={data.distance} onChange={fieldChanged("distance")}/>
-            <input type="time" name="startTime" id="startTime" value={data.startTime} onChange={fieldChanged("startTime")}/>
-            <input type="time" name="endTime" id="endTime" value={data.endTime} onChange={fieldChanged("endTime")}/>
-            <input type="date" name="date" id="date" value={data.date} onChange={fieldChanged("date")}/>
-            <input type="submit" value="Add Run" />
+          <div className="form-inner row">
+            <div className="col-2">
+              <input type="text" className="col-3 form-control" name="desc" id="desc" placeholder="Run Description..." value={data.description} onChange={fieldChanged("description")} />
+            </div>
+            <div className="col-2">
+              <input type="number" className="form-control" name="mileage" id="mileage" placeholder="Miles..." value={data.distance} onChange={fieldChanged("distance")} />
+            </div>
+            <div className="col-2">
+              <input type="time" className="form-control" name="startTime" id="startTime" value={data.startTime} onChange={fieldChanged("startTime")} />
+            </div>
+            <div className="col-2">
+              <input type="time" className="form-control" name="endTime" id="endTime" value={data.endTime} onChange={fieldChanged("endTime")} />
+            </div>
+            <div className="col-2">
+              <input type="date" className="form-control" name="date" id="date" value={data.date} onChange={fieldChanged("date")} />
+            </div>
+            <div className="col-2">
+              <input type="submit" className="btn btn-primary btn-block" value="Add Run" />
+            </div>
           </div>
         </form>
+        <br></br>
       </div>
-    <br></br>
+      <br></br>
 
       <div className="run-list">
         {run.map((entryData, index) => (
-            <RunItem {...entryData} removeRun={removeRun} key={entryData.id} />
+          <RunItem {...entryData} removeRun={removeRun} key={entryData.id} />
         ))}
       </div>
     </>
